a5363dffe2071fdec842421ad7203c4562221136
Mohammed Jareer <mhmmdjareer@gmail.com>
2019-02-23 12:35:14 +0000

    Add abc and var changes snap

---
+++ abc.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Bismillah"
+echo "Shell scripting"
+pwd
---
+++ var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+var1=Jarir
+var2=1986
+var3=chote
+echo "my name is :$var1"
+echo "my number is:$var2"
+echo "me also called as:$var3"
+
